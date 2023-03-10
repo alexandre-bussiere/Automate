84b5e214d27454c7db07f2b46856df6c31835705
aldesse <Bussiere.alexandre@gmail.com>
2023-03-10 13:23:18 +0100

    Vous faites chiez

--- get_from_txt.h
+++ get_from_txt.h
@@ -5,4 +5,10 @@
 #ifndef AUTOMATE_GET_FROM_TXT_H
 #define AUTOMATE_GET_FROM_TXT_H
 
+char*** take_Everyting_FromTxt(void);// -> return toute la ligne n°ligne en forme de tableaux de char*
+char* extractAgreeFormeFromLine(int indexLineToExtract); //-> return la forme fléchis de la ligne envoyer
+char* extractBaseFormeFromLine(int indexLineToExtract); //-> return la forme de base de la ligne envoyer
+char* extractInformationFromLine(int indexLineToExtract); //-> return les informations de la ligne envoyer
+
+
 #endif //AUTOMATE_GET_FROM_TXT_H
--- structure.h
+++ structure.h
@@ -4,14 +4,17 @@
 
 #ifndef AUTOMATE2_STRUCTURE_H
 #define AUTOMATE2_STRUCTURE_H
+
 typedef struct Etat{
     char* nom;
-    struct liaisons** listLiaisons;
-    int* listnbvoisins;
+    struct Transions** listTransitions;
+    int* listnbTransitions;
 }Etat;
-typedef struct liaisons{
+typedef struct Transions{
     Etat* data;
-    struct liaisons* next;
-}liaisons;
-typedef struct liaisons listEtat;
+    struct Transions* next;
+}Transitions;
+typedef struct Transions listEtat;
+
 #endif //AUTOMATE2_STRUCTURE_H
+
