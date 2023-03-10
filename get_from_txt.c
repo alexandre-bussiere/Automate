e8fbe3163daad350129c460ccf5e950dc28c1aaf
aldesse <Bussiere.alexandre@gmail.com>
2023-03-10 09:27:42 +0100

    add get_from_txt.c/.h

---
+++ get_from_txt.c
@@ -0,0 +1,5 @@
+//
+// Created by bussi on 10/03/2023.
+//
+
+#include "get_from_txt.h"
---
+++ get_from_txt.h
@@ -0,0 +1,8 @@
+//
+// Created by bussi on 10/03/2023.
+//
+
+#ifndef AUTOMATE_GET_FROM_TXT_H
+#define AUTOMATE_GET_FROM_TXT_H
+
+#endif //AUTOMATE_GET_FROM_TXT_H
