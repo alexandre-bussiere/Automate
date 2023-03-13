6a10dcc76947d8e7beab35c3f06bd1b20c266988
albanecoiffe <albane.coiffe@efrei.net>
2023-03-13 16:13:32 +0100

    premier commit youpi

---
+++ complet.c
@@ -0,0 +1,16 @@
+//
+// Created by Albane Coiffe on 13/03/2023.
+//
+
+#include "complet.h"
+#include <stdbool.h>
+
+bool IsComplet (listEtat automate ) {
+    bool comlet = true,
+    while ((nombre.next != NULL) && (complet != false) ){
+        int i =0;
+        while ((automate.data->listnbliaison[i] != 0) &&()){
+            i++;
+        }
+    }
+}
---
+++ complet.h
@@ -0,0 +1,10 @@
+//
+// Created by Albane Coiffe on 13/03/2023.
+//
+
+#ifndef AUTOMATE_COMPLET_H
+#define AUTOMATE_COMPLET_H
+#include "structure.h"
+
+bool IsComplet (listEtat A);
+#endif //AUTOMATE_COMPLET_H
