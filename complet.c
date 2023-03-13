a155a225125499398fbd1371e50825bcb5f7ec21
albanecoiffe <albane.coiffe@efrei.net>
2023-03-13 17:15:41 +0100

    jsuis trop contente

--- complet.c
+++ complet.c
@@ -6,11 +6,15 @@
 #include <stdbool.h>
 
 bool IsComplet (listEtat automate ) {
-    bool comlet = true,
+    bool complet = true,
+    int nbLettre = 3
     while ((nombre.next != NULL) && (complet != false) ){
         int i =0;
-        while ((automate.data->listnbliaison[i] != 0) &&()){
+        while ((automate.data->listnbliaison[i] != 0) &&(i<nbLettre - 1)){
             i++;
+        }
+        if (automate.data->listnbliaison[i] != 0){
+            complet = false;
         }
     }
 }
