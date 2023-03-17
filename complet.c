c7045fa616bee85e263d71a329dfb4caa4c58efc
Maelwenn <maelwenn@labidurie.fr>
2023-03-17 10:12:39 +0100

    correction par ce que Albane est pas doué

--- complet.c
+++ complet.c
@@ -3,17 +3,19 @@
 //
 
 #include "complet.h"
+#include "structure.h"
 #include <stdbool.h>
+#include <stdio.h>
 
 bool IsComplet (listEtat automate ) {
-    bool complet = true,
-    int nbLettre = 3
+    bool complet = true;
+    int nbLettre = 3;
     while ((nombre.next != NULL) && (complet != false) ){
         int i =0;
-        while ((automate.data->listnbliaison[i] != 0) &&(i<nbLettre - 1)){
+        while ((automate.data->listnbTransitions[i] != 0) &&(i<nbLettre - 1)){
             i++;
         }
-        if (automate.data->listnbliaison[i] != 0){
+        if (automate.data->listnbTransitions[i] != 0){
             complet = false;
         }
     }
